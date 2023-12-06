badb4fa386873988d3a1f01723a35d01263d2a3b
matgoms <mat.goms@gmail.com>
2023-12-06 16:49:01 -0300

    about

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,11 +4,10 @@ import Footer from "../components/Footer";
 
 const About = () => {
     const HeroSmallTitle = [
-      { title: 'Learn a Little About Me', description: "This is an album filled with my favorite heartwarming travel memories, incredible landscapes i've encountered, pre-wedding shoot, portraits and much more." },
+      { title: 'Learn a Little About Me', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam nesciunt sunt quis nostrum atque, ipsa ullam molestias, reiciendis, in perferendis exercitationem." },
       /* { title: 'Title 2', description: 'Description 2' }, */
       // Add more items as needed
     ];
-  
     return (
         <>
          <div class="bg-white dark:bg-slate-800">
