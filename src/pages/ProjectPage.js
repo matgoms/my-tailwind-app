8e5b5fedae4303138c2b70144319dbe3acbad1c9
matgoms <mat.goms@gmail.com>
2023-12-13 12:03:37 -0300

    project page new sections

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -25,7 +25,7 @@ const ProjectPage = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  
+
   const { projectName } = useParams();
 
 
@@ -47,17 +47,110 @@ const ProjectPage = () => {
     <div class="bg-white dark:bg-slate-800">
       <Menu />
       <ImageFull image={project.image} altText={project.title} />
-      <div class="px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6 lg:gap-8 xl:gap-0 lg:py-32 grid lg:grid grid-cols-5">
-        <div class="max-w-screen-md mb-8 lg:mb-16 col-span-4">
-          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">{project.title}</h2>
-          <p class="text-gray-500 sm:text-xl dark:text-gray-400">{project.description}</p>
-        </div>
-        <div class="max-w-screen-md mb-8 lg:mb-16">
-          <h3 class="mb-4 text-gray-500 dark:text-gray-400"><label class="font-bold text-gray-900 dark:text-gray-200">Year:</label> <br/>{project.year}</h3>
-          <h3 class="mb-4 text-gray-500 dark:text-gray-400"><label class="font-bold text-gray-900 dark:text-gray-200">Scope:</label> <br/>{project.scope}</h3>
-          <h3 class="mb-4 text-gray-500 dark:text-gray-400"><label class="font-bold text-gray-900 dark:text-gray-200">Duration:</label> <br/>{project.duration}</h3>
+      <div class="backdrop-blur-md bg-white/30 mx-auto max-w-screen-xl sm:py-16 lg:px-6 lg:gap-8 xl:gap-8 lg:pt-32">
+        <div class="grid lg:grid grid-cols-5">
+          <div class="max-w-screen-md mb-8 lg:mb-16 col-span-4">
+            <h2 class="mb-4 text-5xl tracking-tight font-extrabold text-gray-900 dark:text-white">{project.title}</h2>
+            <p class="text-gray-500 sm:text-xl dark:text-gray-400">{project.description}</p>
+          </div>
+          <div class="max-w-screen-md mb-8 lg:mb-16">
+            <h3 class="mb-4 text-gray-500 dark:text-gray-400"><label class="font-bold text-gray-900 dark:text-gray-200">Year:</label> <br />{project.year}</h3>
+            <h3 class="mb-4 text-gray-500 dark:text-gray-400"><label class="font-bold text-gray-900 dark:text-gray-200">Scope:</label> <br />{project.scope}</h3>
+            <h3 class="mb-4 text-gray-500 dark:text-gray-400"><label class="font-bold text-gray-900 dark:text-gray-200">Duration:</label> <br />{project.duration}</h3>
+          </div>
         </div>
       </div>
+      <hr class="border-gray-200 mx-auto dark:border-gray-700" />
+
+      <div class="mx-auto w-full sm:py-16 lg:py-32">
+        <div class="flex justify-between mx-auto ">
+          <article class="mx-auto w-full">
+
+            {project.contents &&
+              project.contents.map((content, index) => (
+
+
+                <div key={index}>
+                  {content.title && (
+                    <>
+                      <section>
+                        <div className="lg:max-w-3xl mx-auto w-full">
+                          <h2 className="mt-16 mb-6 text-2xl font-bold text-gray-900 dark:text-white">{content.title}</h2>
+                        </div>
+                      </section>
+                    </>
+                  )}
+                  {content.description && (
+                    <>
+                      <section>
+                        <div className="lg:max-w-3xl mx-auto w-full ">
+                          <p className="mb-6 text-gray-500 text-xl dark:text-gray-400">{content.description}</p>
+                        </div>
+                      </section>
+                    </>
+                  )}
+                  {content.img && (
+                    <>
+                      <section>
+                        <div className="lg:max-w-7xl mx-auto w-full lg:py-16">
+                          <img src={content.img} alt="" className="w-full " />
+
+                        </div>
+                      </section>
+                    </>
+                  )}
+                  {content.imgBig && (
+                    <>
+                      <section>
+                        <div className="lg:max-w-screen-2xl mx-auto w-full lg:py-16">
+                          <img src={content.imgBig} alt="" className="w-full " />
+
+                        </div>
+                      </section>
+                    </>
+                  )}
+
+                  {content.Duo && (
+                    <>
+                      <section>
+                        <div className="max-w-screen-xl py-16 mx-auto columns-1 lg:columns-2 lg:gap-32">
+
+                          {content.Duo.map((duo, duoIndex) => (
+                            <div key={duoIndex} className="grid max-w-screen-2xl mx-auto">
+                              <div className="mr-auto place-self-center w-full">
+                                <img src={duo.imgDuo} alt="" className="w-full" />
+                              </div>
+                            </div>
+                          ))} 
+                          
+                          </div>
+                      </section>
+                    </>
+                  )
+
+                  }
+
+
+
+                  {content.embed && (
+                    <>
+                      <section>
+                        <div className="mr-auto w-full lg:max-w-screen-2xl lg:py-16 mx-auto">
+                          <iframe src={content.embed + '%26scaling%3Dscale-down-width%26page-id%3D0%253A1%26mode%3Ddesign&hide-ui=1'} allowfullscreen class="w-full" height={'920'}></iframe>
+                          <h6 class="mt-4 text-slate-400">{content.caption}</h6>
+                        </div>
+                      </section>
+                    </>
+                  )}
+                </div>
+              ))}
+
+
+          </article>
+        </div>
+
+      </div>
+
       <Footer />
     </div>
 
--- src/projects/uxui/Genera.js
+++ src/projects/uxui/Genera.js
@@ -8,6 +8,38 @@ const Genera = {
     description: 'My design projects showcase companies and startups committed to simplifying bureaucracy, crafting user-friendly training apps, revealing ancestral stories through personalized platforms, and more.',
     scope: 'User Interface, User Experience', 
     duration: '1 Month',
+
+    contents: [
+      {
+        title: 'New Design System',
+        description: "Blending modern genetic concepts with user-friendly interfaces, this project ensures a cohesive visual identity and effortless user experience. The main goal of this design system is to simplify the communication of complex genealogical information, making interactive engagement smoother while elevating the company's brand presence in the genetics field.   ",
+        img: 'https://www.pixground.com/wp-content/uploads/2023/06/Windows-11-White-Abstract-Background-4K-Wallpaper.jpg',
+      },
+      {
+      imgBig: 'https://www.pixground.com/wp-content/uploads/2023/06/Windows-11-White-Abstract-Background-4K-Wallpaper.jpg',
+      },
+     
+
+      {
+        Duo:
+          [
+            {
+              imgDuo: 'https://cdn.photoswipe.com/photoswipe-demo-images/photos/10/img-2500.jpg',
+            },
+            {
+              imgDuo: 'https://cdn.photoswipe.com/photoswipe-demo-images/photos/11/img-2500.jpg',
+            },
+        ] 
+      },
+      {
+        title: 'Protótipo',
+        description: 'Descrição do conteúdo 1.',
+        embed: 'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F19JvWq1o5248YNm5m2Gy4f%2FDesign-System---Genera%3Ftype%3Ddesign%26node-id%3D118-3516%26t%3D0foJRkUZbG7MQN2m-1',
+        caption: '(Scroll down to display everything, press F to fullscreen)',
+      },
+      
+      // Adicione mais itens de conteúdo conforme necessário
+    ],
     };
   
   //para botar imagens, https://www.shecodes.io/athena/38751-how-to-import-all-images-of-a-folder-into-a-react-component
