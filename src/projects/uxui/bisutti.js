2c3e3c718d3118041c5d20eca9109fa14ae0fc3d
matgoms <mat.goms@gmail.com>
2024-01-30 16:53:26 -0300

    more projects

---
+++ src/projects/uxui/bisutti.js
@@ -0,0 +1,40 @@
+const Bisutti = { 
+    title: 'Bisutti', 
+    year: '2018',
+    link: "grupobisutti.com.br",
+    color: "bg-white/80 dark:bg-slate-900",
+    image: process.env.PUBLIC_URL + '/img/' + 'Bisutti/Bisutti.jpg',
+    thumb: process.env.PUBLIC_URL + '/img/' + 'Bisutti/Bisutti-thumb.png',
+    description: "The Bisutti Group is a benchmark brand in catering for social and corporate events.",
+    scope: "User Interface",
+    duration: '2 Month',
+
+    contents: [
+        
+       
+        {
+          img: process.env.PUBLIC_URL + '/img/' + 'Bisutti/style.png',
+        },
+        {
+            title: "The new visual identity",
+            description: "The idea behind choosing these colors was to capture the luxury and purity associated with weddings and events. We aimed for a timeless and elegant feel, creating a digital atmosphere that seamlessly connects the online experience with our real-world events.",
+        },
+        {
+        description: "Even though the website design didn't get approval, the concept highlights the commitment to excellence. The use of gold and white tones, along with stylish and soft fonts, aimed to elevate the digital presence to match the grandeur of the events they hosted.",
+        },
+        {
+            imgBig: process.env.PUBLIC_URL + '/img/' + 'Bisutti/home.png',
+          },
+          {
+            img: process.env.PUBLIC_URL + '/img/' + 'Bisutti/interna.png',
+          },
+        // Adicione mais itens de conteúdo conforme necessário
+      ],
+};
+  
+  
+
+
+export default Bisutti;
+  
+
