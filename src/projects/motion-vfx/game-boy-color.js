8a11771da6d59c16e877b82347312a6309b81f72
matgoms <mat.goms@gmail.com>
2024-03-15 17:05:07 -0300

    new 3d project

---
+++ src/projects/motion-vfx/game-boy-color.js
@@ -0,0 +1,29 @@
+const Coffee = { 
+    title: 'Game Boy Color', 
+    year: "2024",
+    color: "bg-slate-700 dark:bg-slate-900",
+    image: `${process.env.PUBLIC_URL}/img/gameboy/gameboy.jpg`,
+    thumb: `${process.env.PUBLIC_URL}/img/gameboy/gameboy-thumb.jpg`,
+    description: "This project is about Game Boy Color, an icon of the gamer culture of the 90s. It was amazing to be able to model and animate this beauty in Blender.",
+    scope: '3D Modeling, 3D Animation, Motion Design', 
+    
+  contents: [
+
+    
+    {
+        html:
+          '<video className="w-full" autoplay loop muted playsinline><source src="../img/gameboy/gameboy.mp4" type="video/mp4" /></video>',
+        /* canva:
+          "", */
+
+      },
+
+    ]
+    };
+  
+  
+
+
+export default Coffee;
+  
+
